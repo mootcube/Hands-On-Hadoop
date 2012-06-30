4ee87e927d6392c08dd10edf531612901a65a200
MoOT <mathieu.chataigner@gmail.com>
2012-06-30 18:37:14 +0200

    package success ex1/ex2

--- src/main/java/com/soat/hadoopworkshop/ex3/MostFrequentMapper.java
+++ src/main/java/com/soat/hadoopworkshop/ex3/MostFrequentMapper.java
@@ -6,10 +6,13 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import java.util.StringTokenizer;
 
 public class MostFrequentMapper extends
 				    Mapper<LongWritable, Text, IntWritable, Text> {
     
+    private static final IntWritable one=new IntWritable(1);
+
     @Override
 	public void map(LongWritable key, Text value, Context context)
 	throws IOException, InterruptedException 
